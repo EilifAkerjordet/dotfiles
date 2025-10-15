eac8d82c0ca2c306a317e32debab114135f5e04b
Eilif Akerjordet <eilif@akademikernepluss.no>
2025-10-15 15:50:00 +0200

    Open dadbodui in zenmode

--- .config/nvim/lua/plugins/terminals.lua
+++ .config/nvim/lua/plugins/terminals.lua
@@ -52,7 +52,14 @@ return {
       {
         "<leader>D",
         function()
-          Snacks.terminal.toggle({ "nvim", "-c", "DBUI" }, {
+          Snacks.terminal.toggle({
+            "nvim",
+            "-c",
+            -- To turn off disturbing UI elements making it feel more like a "native" TUI app (not running in nvim)
+            "autocmd VimEnter * lua vim.defer_fn(function() vim.opt.laststatus = 0 vim.opt.cmdheight = 0 vim.opt.showtabline = 0 vim.opt.number = false vim.opt.relativenumber = false vim.opt.signcolumn = 'no' vim.opt.foldcolumn = '0' vim.opt.fillchars = { eob = ' ' } end, 0)",
+            "-c",
+            "DBUI",
+          }, {
             env = { SNACKS_TERMINAL_ID = "dadbod" },
             win = { position = "float", height = 0.99, width = 0.99, border = "rounded" },
           })
