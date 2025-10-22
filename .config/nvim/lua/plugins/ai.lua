42214cf81e85c85a0b2d71f635832a0060dcb24f
Eilif Akerjordet <eilif@akademikernepluss.no>
2025-10-22 11:12:52 +0200

    use claude code as main ai driver

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,119 +1,119 @@
 return {
-  {
-    "NickvanDyke/opencode.nvim",
-    dependencies = {
-      -- Recommended for `ask()` and `select()`.
-      -- Required for `toggle()`.
-      { "folke/snacks.nvim", opts = { input = {}, picker = {} } },
-    },
-    config = function()
-      vim.g.opencode_opts = {
-        terminal = {
-          win = {
-            position = "float",
-            width = 0.99,
-            height = 0.99,
-            border = "rounded",
-            backdrop = 80,
-            enter = true,
-          },
-        },
-      }
-
-      require("opencode.config").opts.terminal.win.keys = {
-        hide = {
-          "<leader>A",
-          function(self)
-            self:hide()
-          end,
-          mode = "t",
-          desc = "Hide opencode",
-        },
-      }
-
-      vim.o.autoread = true
-
-      vim.keymap.set("n", "<leader>A", function()
-        require("opencode").toggle()
-      end, { desc = "Toggle opencode" })
-      vim.keymap.set({ "n", "x" }, "<leader>oa", function()
-        require("opencode").ask("@this: ", { submit = true })
-      end, { desc = "Ask about this" })
-      vim.keymap.set({ "n", "x" }, "<leader>os", function()
-        require("opencode").select()
-      end, { desc = "Select prompt" })
-      vim.keymap.set({ "n", "x" }, "<leader>o+", function()
-        require("opencode").prompt("@this")
-      end, { desc = "Add this" })
-      vim.keymap.set("n", "<leader>ot", function()
-        require("opencode").toggle()
-      end, { desc = "Toggle embedded" })
-      vim.keymap.set("n", "<leader>oc", function()
-        require("opencode").command()
-      end, { desc = "Select command" })
-      vim.keymap.set("n", "<leader>on", function()
-        require("opencode").command("session_new")
-      end, { desc = "New session" })
-      vim.keymap.set("n", "<leader>oi", function()
-        require("opencode").command("session_interrupt")
-      end, { desc = "Interrupt session" })
-      vim.keymap.set("n", "<leader>oA", function()
-        require("opencode").command("agent_cycle")
-      end, { desc = "Cycle selected agent" })
-      vim.keymap.set("n", "<S-C-u>", function()
-        require("opencode").command("messages_half_page_up")
-      end, { desc = "Messages half page up" })
-      vim.keymap.set("n", "<S-C-d>", function()
-        require("opencode").command("messages_half_page_down")
-      end, { desc = "Messages half page down" })
-    end,
-  },
-
   -- {
-  --   "coder/claudecode.nvim",
-  --   dependencies = { "folke/snacks.nvim" },
-  --   config = true,
-  --   lazy = false,
-  --   keys = {
-  --     { "<leader>a", nil, desc = "AI/Claude Code" },
-  --     { "<leader>A", "<cmd>ClaudeCodeFocus<cr>", desc = "Claude Code", mode = { "n", "x" } },
-  --     { "<leader>ar", "<cmd>ClaudeCode --resume<cr>", desc = "Resume Claude" },
-  --     { "<leader>aC", "<cmd>ClaudeCode --continue<cr>", desc = "Continue Claude" },
-  --     { "<leader>am", "<cmd>ClaudeCodeSelectModel<cr>", desc = "Select Claude model" },
-  --     { "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
-  --     { "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
-  --
-  --     {
-  --       "<leader>as",
-  --       "<cmd>ClaudeCodeTreeAdd<cr>",
-  --       desc = "Add file",
-  --       ft = { "NvimTree", "neo-tree", "oil", "minifiles" },
-  --     },
-  --     -- Diff management
-  --     { "<leader>aa", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
-  --     { "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
+  --   "NickvanDyke/opencode.nvim",
+  --   dependencies = {
+  --     -- Recommended for `ask()` and `select()`.
+  --     -- Required for `toggle()`.
+  --     { "folke/snacks.nvim", opts = { input = {}, picker = {} } },
   --   },
-  --   opts = {
-  --     terminal_cmd = "/Users/eilifakerjordet/.claude/local/claude",
-  --     terminal = {
-  --       snacks_win_opts = {
-  --         position = "float",
-  --         width = 0.99,
-  --         height = 0.99,
-  --         border = "rounded",
-  --         backdrop = 80,
-  --         keys = {
-  --           claude_hide = {
-  --             "<leader>A",
-  --             function(self)
-  --               self:hide()
-  --             end,
-  --             mode = "t",
-  --             desc = "Hide",
-  --           },
+  --   config = function()
+  --     vim.g.opencode_opts = {
+  --       terminal = {
+  --         win = {
+  --           position = "float",
+  --           width = 0.99,
+  --           height = 0.99,
+  --           border = "rounded",
+  --           backdrop = 80,
+  --           enter = true,
   --         },
   --       },
-  --     },
-  --   },
+  --     }
+  --
+  --     require("opencode.config").opts.terminal.win.keys = {
+  --       hide = {
+  --         "<leader>A",
+  --         function(self)
+  --           self:hide()
+  --         end,
+  --         mode = "t",
+  --         desc = "Hide opencode",
+  --       },
+  --     }
+  --
+  --     vim.o.autoread = true
+  --
+  --     vim.keymap.set("n", "<leader>A", function()
+  --       require("opencode").toggle()
+  --     end, { desc = "Toggle opencode" })
+  --     vim.keymap.set({ "n", "x" }, "<leader>oa", function()
+  --       require("opencode").ask("@this: ", { submit = true })
+  --     end, { desc = "Ask about this" })
+  --     vim.keymap.set({ "n", "x" }, "<leader>os", function()
+  --       require("opencode").select()
+  --     end, { desc = "Select prompt" })
+  --     vim.keymap.set({ "n", "x" }, "<leader>o+", function()
+  --       require("opencode").prompt("@this")
+  --     end, { desc = "Add this" })
+  --     vim.keymap.set("n", "<leader>ot", function()
+  --       require("opencode").toggle()
+  --     end, { desc = "Toggle embedded" })
+  --     vim.keymap.set("n", "<leader>oc", function()
+  --       require("opencode").command()
+  --     end, { desc = "Select command" })
+  --     vim.keymap.set("n", "<leader>on", function()
+  --       require("opencode").command("session_new")
+  --     end, { desc = "New session" })
+  --     vim.keymap.set("n", "<leader>oi", function()
+  --       require("opencode").command("session_interrupt")
+  --     end, { desc = "Interrupt session" })
+  --     vim.keymap.set("n", "<leader>oA", function()
+  --       require("opencode").command("agent_cycle")
+  --     end, { desc = "Cycle selected agent" })
+  --     vim.keymap.set("n", "<S-C-u>", function()
+  --       require("opencode").command("messages_half_page_up")
+  --     end, { desc = "Messages half page up" })
+  --     vim.keymap.set("n", "<S-C-d>", function()
+  --       require("opencode").command("messages_half_page_down")
+  --     end, { desc = "Messages half page down" })
+  --   end,
   -- },
+
+  {
+    "coder/claudecode.nvim",
+    dependencies = { "folke/snacks.nvim" },
+    config = true,
+    lazy = false,
+    keys = {
+      { "<leader>a", nil, desc = "AI/Claude Code" },
+      { "<leader>A", "<cmd>ClaudeCodeFocus<cr>", desc = "Claude Code", mode = { "n", "x" } },
+      { "<leader>ar", "<cmd>ClaudeCode --resume<cr>", desc = "Resume Claude" },
+      { "<leader>aC", "<cmd>ClaudeCode --continue<cr>", desc = "Continue Claude" },
+      { "<leader>am", "<cmd>ClaudeCodeSelectModel<cr>", desc = "Select Claude model" },
+      { "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
+      { "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
+
+      {
+        "<leader>as",
+        "<cmd>ClaudeCodeTreeAdd<cr>",
+        desc = "Add file",
+        ft = { "NvimTree", "neo-tree", "oil", "minifiles" },
+      },
+      -- Diff management
+      { "<leader>aa", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
+      { "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
+    },
+    opts = {
+      terminal_cmd = "/Users/eilifakerjordet/.claude/local/claude",
+      terminal = {
+        snacks_win_opts = {
+          position = "float",
+          width = 0.99,
+          height = 0.99,
+          border = "rounded",
+          backdrop = 80,
+          keys = {
+            claude_hide = {
+              "<leader>A",
+              function(self)
+                self:hide()
+              end,
+              mode = "t",
+              desc = "Hide",
+            },
+          },
+        },
+      },
+    },
+  },
 }
