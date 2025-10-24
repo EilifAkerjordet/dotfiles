66f58d206b7a7fdc9ba7e96bd0b6b944cfc09147
Eilif Akerjordet <eilif@akademikernepluss.no>
2025-10-24 12:44:34 +0200

    disable inlay hints

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,6 +2,7 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      inlay_hints = { enabled = false },
       servers = {
         -- Tailwind with custom classRegex for Blazor Cn() function
         tailwindcss = {
